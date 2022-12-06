375d6b89b051a6cc8a708a7f55dc2726dad0e7bf
vincentappiah <appiahv@rocketmail.com>
2022-12-07 00:21:56 +0100

    Add files via upload

--- Tutorials/Episode-2/trim.sh
+++ Tutorials/Episode-2/trim.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 mkdir trimmed
-porechop -t 4 -i data/bulk_bottom.fastq.gz  -o trimmed/bulk_bottom.trimmed.fastq.gz --format fastq.gz
-porechop -t 4 -i data/rhizosphere_bottom.fastq.gz  -o trimmed/rhizosphere_bottom.trimmed.fastq.gz --format fastq.gz
-porechop -t 4 -i data/bulk_top.fastq.gz  -o trimmed/bulk_top.fastq.gz --format fastq.gz
-porechop -t 4 -i data/rhizosphere_top.fastq.gz  -o trimmed/rhizosphere_top.fastq.gz --format fastq.gz
+porechop -i data/bulk_bottom.fastq.gz  -o trimmed/bulk_bottom.trimmed.fastq.gz --format fastq.gz
+porechop -i data/rhizosphere_bottom.fastq.gz  -o trimmed/rhizosphere_bottom.trimmed.fastq.gz --format fastq.gz
+porechop -i data/bulk_top.fastq.gz  -o trimmed/bulk_top.fastq.gz --format fastq.gz
+porechop -i data/rhizosphere_top.fastq.gz  -o trimmed/rhizosphere_top.fastq.gz --format fastq.gz
