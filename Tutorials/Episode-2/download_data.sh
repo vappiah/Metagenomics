b107f14925587a3c0bbc2aff6648e677cc8537b5
vincentappiah <appiahv@rocketmail.com>
2022-12-07 00:19:38 +0100

    Add files via upload

---
+++ Tutorials/Episode-2/download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+wget https://zenodo.org/record/4274812/files/bulk_bottom.fastq.gz
+wget https://zenodo.org/record/4274812/files/bulk_top.fastq.gz
+wget https://zenodo.org/record/4274812/files/rhizosphere_bottom.fastq.gz
+wget https://zenodo.org/record/4274812/files/rhizosphere_top.fastq.gz
+
