dc6a3ab02ae2ff210d1b15d90b313f6c930f1272
vincentappiah <appiahv@rocketmail.com>
2022-12-07 15:49:01 +0000

    Update classify_reads.sh

--- Tutorials/Episode-2/classify_reads.sh
+++ Tutorials/Episode-2/classify_reads.sh
@@ -9,9 +9,7 @@ krakendb=minikraken2_v2_8GB_201904_UPDATE
 
 kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_bottom.report --gzip-compressed filtered_reads/bulk_bottom.filt.fastq.gz > kraken_reports/bulk_bottom.kraken
 
-
 kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/rhizosphere_bottom.report --gzip-compressed filtered_reads/rhizosphere_bottom.filt.fastq.gz > kraken_reports/rhizosphere_bottom.kraken
-
 
 kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_top.report --gzip-compressed filtered_reads/bulk_top.filt.fastq.gz > kraken_reports/bulk_top.kraken
 
