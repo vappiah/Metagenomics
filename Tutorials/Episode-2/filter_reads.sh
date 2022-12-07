d9d161be14378a1a515c433dac5542bbb2642606
vincentappiah <appiahv@rocketmail.com>
2022-12-07 10:59:24 +0100

    Add files via upload

--- Tutorials/Episode-2/filter_reads.sh
+++ Tutorials/Episode-2/filter_reads.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-mkdir trimmed
-porechop -i data/bulk_bottom.fastq.gz  -o trimmed/bulk_bottom.trimmed.fastq.gz --format fastq.gz
-porechop -i data/rhizosphere_bottom.fastq.gz  -o trimmed/rhizosphere_bottom.trimmed.fastq.gz --format fastq.gz
-porechop -i data/bulk_top.fastq.gz  -o trimmed/bulk_top.fastq.gz --format fastq.gz
-porechop -i data/rhizosphere_top.fastq.gz  -o trimmed/rhizosphere_top.fastq.gz --format fastq.gz
+mkdir filtered_reads
+
+fastp -q 9 -l 1000 -b 2000 -G -A -i trimmed/bulk_bottom.trimmed.fastq.gz -o filtered_reads/bulk_bottom.filt.fastq.gz
+fastp -q 9 -l 1000 -b 2000 -G -A -i trimmed/rhizosphere_bottom.trimmed.fastq.gz -o filtered_reads/rhizosphere_bottom.filt.fastq.gz
+fastp -q 9 -l 1000 -b 2000 -G -A -i trimmed/bulk_top.trimmed.fastq.gz -o filtered_reads/bulk_top.filt.fastq.gz
+fastp -q 9 -l 1000 -b 2000 -G -A -i  trimmed/rhizosphere_top.trimmed.fastq.gz -o filtered_reads/rhizosphere_top.filt.fastq.gz
+
+#-i input file
+#-o output file
+#-q minimum quality
+#-l length required
+#-b maximum length
+#-A disable adapter trimming
+#-G disable polyG tail trimming
