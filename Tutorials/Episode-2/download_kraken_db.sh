13c9e69d36dcc567d5f5276d92cffd265d302b79
vincentappiah <appiahv@rocketmail.com>
2022-12-07 15:01:02 +0000

    Update download_kraken_db.sh

--- Tutorials/Episode-2/download_kraken_db.sh
+++ Tutorials/Episode-2/download_kraken_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-downloadlink=ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/old/minikraken2_v2_8GB_201904.tgz
-tar xvfz minikraken2_v2_8GB_201904.tgz
 
+wget ftp://ftp.ccb.jhu.edu/pub/data/kraken2_dbs/old/minikraken2_v2_8GB_201904.tgz 
+tar xvfz minikraken2_v2_8GB_201904.tgz
