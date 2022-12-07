8e2558107ef74414cc8f64be8813d034e22e5b2b
vincentappiah <appiahv@rocketmail.com>
2022-12-07 15:14:01 +0100

    Add files via upload

--- Tutorials/Episode-2/classify_reads.sh
+++ Tutorials/Episode-2/classify_reads.sh
@@ -7,14 +7,14 @@ mkdir kraken_reports
 
 krakendb=minikraken2_v2_8GB_201904_UPDATE
 
-kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_bottom.report --gzip-compressed filtered_reads/bulk_bottom.filt.fastq.gz >bulk_bottom.kraken
+kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_bottom.report --gzip-compressed filtered_reads/bulk_bottom.filt.fastq.gz > kraken_reports/bulk_bottom.kraken
 
 
-kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/rhizosphere_bottom.report --gzip-compressed filtered_reads/rhizosphere_bottom.filt.fastq.gz >rhizosphere_bottom.kraken
+kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/rhizosphere_bottom.report --gzip-compressed filtered_reads/rhizosphere_bottom.filt.fastq.gz > kraken_reports/rhizosphere_bottom.kraken
 
 
-kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_top.report --gzip-compressed filtered_reads/bulk_top.filt.fastq.gz >bulk_top.kraken
+kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/bulk_top.report --gzip-compressed filtered_reads/bulk_top.filt.fastq.gz > kraken_reports/bulk_top.kraken
 
-kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/rhizosphere_top.report --gzip-compressed filtered_reads/rhizosphere_top.filt.fastq.gz >rhizosphere_top.kraken
+kraken2 --use-names --db $krakendb --threads 4 --report kraken_reports/rhizosphere_top.report --gzip-compressed filtered_reads/rhizosphere_top.filt.fastq.gz > kraken_reports/rhizosphere_top.kraken
 
 
