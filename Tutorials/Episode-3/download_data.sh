2bccc25e2bff39076bb5ec2763a2cc994c324686
vincentappiah <appiahv@rocketmail.com>
2022-12-12 10:39:55 +0100

    Update and rename r.md to download_data.sh

---
+++ Tutorials/Episode-3/download_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+
+wget https://zenodo.org/record/6958893/files/ERR2231567_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231567_Read_2.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231568_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231568_Read_2.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231569_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231569_Read_2.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231570_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231570_Read_2.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231571_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231571_Read_2.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231572_Read_1.fastqsanger.gz
+wget https://zenodo.org/record/6958893/files/ERR2231572_Read_2.fastqsanger.gz
